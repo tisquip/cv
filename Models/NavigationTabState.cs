e3aa3a220218c535f3dd945cef83b72e7cfd240d
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2021-05-13 15:54:49 +0200

    Added tabs

---
+++ Models/NavigationTabState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyResumeSite.Models
+{
+    public enum NavigationTabState
+    {
+        Main,
+        LiveMatches,
+        Standings,
+        Schedule
+    }
+}
