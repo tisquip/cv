2f099ddb55c3927aac045e343838cc22251ca83e
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2021-05-15 10:48:41 +0200

    Time Zones

---
+++ Pages/ComponentOptions/TimeZoneComponentOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyResumeSite.Pages.ComponentOptions
+{
+    public class TimeZoneComponentOptions
+    {
+        public DateTime DateTimeUtc { get; set; }
+        public bool UseSmallClass { get; set; }
+        public TimeZoneComponentOptions(string dateTimeStringUtc, bool useSmallClass = true)
+        {
+            DateTime.TryParse(dateTimeStringUtc, out DateTime parsedDateTime);
+            DateTimeUtc = parsedDateTime;
+            UseSmallClass = useSmallClass;
+        }
+
+        public TimeZoneComponentOptions(DateTime dateTimeUtc, bool useSmallClass = true)
+        {
+            DateTimeUtc = dateTimeUtc;
+            UseSmallClass = useSmallClass;
+        }
+    }
+}
